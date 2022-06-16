4f71e8719dcd109ee53281244797a0fd8b49f7c5
wkevin <wkevin27@gmail.com>
2022-06-16 20:41:32 +0800

    fix: 适配 python3.8, 3.9, 3.10

--- backend/python/helper.go
+++ backend/python/helper.go
@@ -6,3 +6,33 @@ package python
 #include <Python.h>
 */
 import "C"
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type Info struct {
+	version string
+}
+
+func Version_Check(ver string) bool {
+	_ver := Py_GetVersion()
+	matched, err := regexp.Match(ver, []byte(_ver))
+	if err == nil {
+		return matched
+	}
+	return false
+}
+
+func PyErr_Occurred() string {
+	errstr := ""
+	if err := C.PyErr_Occurred(); togo(err) != nil {
+		fmt.Println("------ python exception ------")
+		C.PyErr_Print()
+		fmt.Println("------")
+		fmt.Println("sys.path: ", PyImport_GetModule("sys").GetAttrString("path").Repr())
+		fmt.Println("------ end ------")
+	}
+	return errstr
+}
